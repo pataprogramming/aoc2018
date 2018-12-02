76df05b541c063e0ae2850773717a01599daf7e5
Paul L. Snyder <paul@pataprogramming.com>
2018-12-01 19:58:04 -0500

    Day 1 Clojure solution

--- src/aoc2018/core.clj
+++ src/aoc2018/core.clj
@@ -1,8 +1,9 @@
-(ns aoc2018.core)
+(ns aoc2018.core
+  (:require [clojure.string :as string]))
 
-(defn parse-frequency [fs]
-  (let [op-str  (subs fs 0 1)
-        num-str (subs fs 1)
+(defn parse-drift [f]
+  (let [op-str  (subs f 0 1)
+        num-str (subs f 1)
         op      (case op-str
                   "+" +
                   "-" -
@@ -10,9 +11,36 @@
         num     (Integer/parseInt num-str)]
     [op num]))
 
-(defn calibrate-frequencies [start freqs]
-  (reduce (fn [acc [f & fs]]
-            (let [[op num] (parse-frequence f)]
+(defn calibrate-frequency [start offsets]
+  (reduce (fn [acc offset]
+            (let [[op num] (parse-drift offset)]
               (op acc num)))
           start
-          freqs))
+          offsets))
+
+(defn solve-day1-a []
+  (let [input (-> "resources/day1.txt"
+                  slurp
+                  (string/split #"\n"))]
+    (calibrate-frequency 0 input)))
+
+;; Answer: 459
+
+
+(defn stabilize-frequency [start offsets]
+  (reduce (fn [[acc seen?] offset]
+            (let [[op num] (parse-drift offset)
+                  freq     (op acc num)]
+              (if (seen? freq)
+                (reduced freq)
+                [freq (conj seen? freq)])))
+          [start (hash-set)]
+          offsets))
+
+(defn solve-day1-b []
+  (let [input (-> "resources/day1.txt"
+                  slurp
+                  (string/split #"\n"))]
+    (stabilize-frequency 0 (cycle input))))
+
+;; Answer: 65474
